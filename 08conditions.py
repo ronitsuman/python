4921e0ff3d9f22adad19529fdf2b9ef9fa554642
Ronit Suman <ronitsuman59@gmail.com>
2024-05-31 13:50:39 +0530

    xz

---
+++ 08conditions.py
@@ -0,0 +1,21 @@
+# age = 16 ;
+
+# if age >= 18 :
+#     print("you are eligible") 
+# else:
+#     print("you are not eligible ")
+
+# a = input("Enter A : ")
+# b = input("Enter B :" ) ;
+
+# if (a>b):
+#     print("A is greater than B")
+# elif a==b :
+#     print("A is equal to B ")
+# else :
+#     print("B is greater than A")
+
+a = 20 
+b = 15 
+
+print("A") if a>b else print("B")
